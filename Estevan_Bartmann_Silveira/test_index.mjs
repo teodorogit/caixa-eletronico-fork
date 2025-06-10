cb767334d67d0a4d906d46b980139040649b898a
Matheus Vinicius Lula Teodoro <matheus_teodoro@sicredi.com.br>
2025-06-10 10:56:08 -0300

    feat: (testValidation): create cases test to validate function notasASeremSacadas

---
+++ Estevan_Bartmann_Silveira/test_index.mjs
@@ -0,0 +1,40 @@
+const assert = require('node:assert/strict')
+const it = require('node:test')
+const notasASeremSacadas = require('./index.js')
+
+it('Testing when input value its zero', () => {
+  const inputZero = notasASeremSacadas([10])
+  assert.deepEqual(inputZero, [10])
+})
+
+it('Testing with input 100, expecting 1 note 100', () => {
+  const valueExpect = notasASeremSacadas([100])
+  console.log('>>>',valueExpect)
+  assert.deepEqual(valueExpect,[100])
+})
+
+it('Testing with negative value', () => {
+  const valueExpect = notasASeremSacadas(-1)
+ assert.deepEqual(valueExpect,[])
+})
+
+it('Testing with zero', () => {
+  const valueExpect = notasASeremSacadas(0)
+ assert.deepEqual(valueExpect,[])
+})
+
+it('Testing, edge value', () => {
+  const valueExpect = notasASeremSacadas(10000)
+  assert.deepEqual(valueExpect, [
+  100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
+  100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
+  100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
+  100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
+  100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
+  100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
+  100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
+  100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
+  100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
+  100
+])
+})
